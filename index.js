3603a0e71200e981f3205047d753ba0af1012910
cbraissant <chrisbraissant@gmail.com>
2021-10-29 00:31:18 +0200

    tryouts of JWT

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const express = require('express')
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = 3000
+const secret_phrase = "ThisIsMySecretPhrase"
+
+function generateAccessToken(payload) {
+    // creates a new token valid for 1800 seconds (30min)
+    return jwt.sign(payload, secret_phrase, { expiresIn: 1800 })
+}
 
 const isAuthenticated = (req, res, next) => {
-    console.log('middleware')
-    next()
+    try {
+        const authHeader = req.headers.authorization;
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, secret_phrase, function (err, decoded) {
+            if (!err) {
+                console.log(decoded);
+                req.isAuthenticated = true;
+                next()
+            } else {
+                res.status(401).json({ "error": "Invallid token" })
+            }
+        })
+    }
+    catch {
+        res.status(401).json({ "error": "Authentication token not provided" })
+    }
 }
 
 app.get('/', isAuthenticated, (req, res) => {
-    res.send('hello world')
+    let response = { "test": "hello world" }
+    res.json(response)
+})
+
+app.get('/login', (req, res) => {
+    payload = { "email": "c@c.com" }
+    token = generateAccessToken(payload)
+    res.status(200)
+    res.json({ token });
 })
 
 
