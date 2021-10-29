c5e48c4b1130939601d537e48376e56debd0b66c
cbraissant <chrisbraissant@gmail.com>
2021-10-29 10:01:49 +0200

    add basic user flat file storage

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const fs = require('fs')
 const jwt = require('jsonwebtoken');
+
 const app = express()
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
 const port = 3000
 const secret_phrase = "ThisIsMySecretPhrase"
 
@@ -19,7 +26,7 @@ const isAuthenticated = (req, res, next) => {
                 req.isAuthenticated = true;
                 next()
             } else {
-                res.status(401).json({ "error": "Invallid token" })
+                res.status(401).json({ "error": "Invalid token" })
             }
         })
     }
@@ -38,6 +45,29 @@ app.get('/login', (req, res) => {
     token = generateAccessToken(payload)
     res.status(200)
     res.json({ token });
+})
+
+app.post('/register', (req, res) => {
+    let email, password;
+    // the POST request needs to be check before processing the data
+    try {
+        email = req.body.email;
+        password = req.body.password;
+        // TODO: hash password
+        user = { email: email, password: password }
+    }
+    catch {
+        res.status(401).json({ "error": "Invalid argument" })
+    }
+
+    // then the data can be saved to the database
+    try {
+        const data = fs.writeFileSync('./storage/users.json', JSON.stringify(user))
+        res.json(user)
+    }
+    catch (err) {
+        res.send(err)
+    }
 })
 
 
